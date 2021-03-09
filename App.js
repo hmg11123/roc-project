e350fd56f570dded434e381c0c2642bb3020c910
hmg11123 <you@example.com>
2021-03-09 21:35:06 +0900

    roc

--- App.js
+++ App.js
@@ -10,11 +10,67 @@ import { LinearGradient } from "expo-linear-gradient";
 
 const CURRENT_WIDTH = Dimensions.get("window").width;
 
+const ROC_DATUM = ["가위", "바위", "보"];
+
 const App = () => {
  const [tab, setTab] = useState(0);
+ const [mineData, setMineData] = useState(`잠시만 기다려 주세요`);
+ const [cpuData, setCpuData] = useState(`잠시만 기다려 주세요`);
+ const [resultText, setResultText] = useState(``);
+
+ const _getRandomNumber = () => Math.floor(Math.random() * 3);
 
  const _startButtonClickHandler = (value) => {
   setTab(value);
+
+  if (value === 0) {
+   setMineData(`잠시만 기다려 주세요`);
+   setCpuData(`잠시만 기다려 주세요`);
+   setResultText(``);
+  }
+
+  if (value === 1) {
+   const ran1 = _getRandomNumber();
+   const ran2 = _getRandomNumber();
+
+   const mine = ROC_DATUM[ran1];
+   const cpu = ROC_DATUM[ran2];
+   setMineData(mine);
+   setCpuData(cpu);
+
+   if (ran1 === ran2) {
+    setResultText("비겼습니다.");
+    return;
+   }
+
+   if (ran1 === 0) {
+    if (ran2 === 1) {
+     setResultText("컴퓨터한테 졌습니다.");
+     return;
+    } else if (ran2 === 2) {
+     setResultText("컴퓨터한테 이겼습니다.");
+     return;
+    }
+   }
+   if (ran1 === 1) {
+    if (ran2 === 2) {
+     setResultText("컴퓨터한테 졌습니다.");
+     return;
+    } else if (ran2 === 0) {
+     setResultText("컴퓨터한테 이겼습니다.");
+     return;
+    }
+   }
+   if (ran1 === 2) {
+    if (ran2 === 0) {
+     setResultText("컴퓨터한테 졌습니다.");
+     return;
+    } else if (ran2 === 1) {
+     setResultText("컴퓨터한테 이겼습니다.");
+     return;
+    }
+   }
+  }
  };
 
  return (
@@ -31,28 +87,33 @@ const App = () => {
     {tab === 1 && (
      <View>
       <View style={styles.inGameTop}>
-       <Text>top</Text>
+       <Text>{cpuData} </Text>
       </View>
       <View style={styles.inGameMiddle}>
        <LinearGradient
-        colors={[`#079992`, `#38ada9`]}
-        locations={[1, 0.9]}
-        start={[0.25, 0.9]}
-        end={[0.5, 0.4]}
+        // colors={[`#079992`, `#38ada9`]}
+        colors={[`#e74c3c`, `#2980b9`]}
+        locations={[1, 0.99]}
+        start={[0, 0]}
+        end={[0.494, 0.9]}
         style={styles.vsView}
        >
+        <Text style={styles.vsViewText}>컴퓨터</Text>
         <Text style={styles.vsViewText}>VS</Text>
+        <Text style={styles.vsViewText}>사용자</Text>
        </LinearGradient>
       </View>
       <View style={styles.inGameBottom}>
-       <Text>bottom</Text>
+       <Text>{mineData}</Text>
       </View>
      </View>
     )}
     {/** (조건식) && 트루라면 */}
    </View>
    <View style={styles.resultArea}>
-    <View style={styles.resultAreaTop}></View>
+    <View style={styles.resultAreaTop}>
+     <Text>{resultText}</Text>
+    </View>
     <View style={styles.resultAreaBottom}>
      {tab === 1 && (
       <TouchableOpacity
@@ -122,7 +183,8 @@ const styles = StyleSheet.create({
   width: CURRENT_WIDTH,
   height: 50,
   alignItems: "center",
-  justifyContent: "center",
+  justifyContent: "space-around",
+  flexDirection: `row`,
  },
  vsViewText: {
   fontSize: 20,
